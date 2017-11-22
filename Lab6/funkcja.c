08e7f1be703a1351f31be47cb9c11e586cc4cae9
Patryk Szwed <patryk.szwed@outlook.com>
2017-11-22 22:15:03 +0100

    initial project version

---
+++ Lab6/funkcja.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "func.h"
+
+void change2LowerCase(char s)
+{
+    int c;
+
+    while((c = getchar()) != EOF)
+    {
+        if(isupper(c) && c == toupper(s))
+        {
+            printf("%c", tolower(c));
+        }
+        else if(c == '\n')
+        {
+            printf("\n");
+        }
+        else printf("%c", c);
+    }
+}
+void change2UpperCase(char s)
+{
+    int c;
+
+    while((c = getchar()) != EOF)
+    {
+        if(islower(c) && c == tolower(s))
+        {
+            printf("%c", toupper(c));
+        }
+        else if(c == '\n')
+        {
+            printf("\n");
+        }
+        else printf("%c", c);
+    }
+}
+void switchCase(char s)
+{
+    int c;
+
+    while((c = getchar()) != EOF)
+    {
+        if(isupper(c) && c == toupper(s))
+        {
+            printf("%c", tolower(c));
+        }
+        else if(islower(c) && c == tolower(s))
+        {
+            printf("%c", toupper(c));
+        }
+        else if(c == '\n')
+        {
+            printf("\n");
+        }
+        else printf("%c", c);
+    }
+}
+void makeNames(char s)
+{
+    puts("makenames");
+}
