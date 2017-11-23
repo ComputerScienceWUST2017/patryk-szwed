4e335126dacf6c52ee2645cad793f5794f541f2c
Patryk Szwed <patryk.szwed@outlook.com>
2017-11-23 02:11:38 +0100

    Task 1 from lab6 is added

---
+++ patryk-szwed/Lab6/func.h
@@ -0,0 +1,9 @@
+#ifndef FUNC_H_INCLUDED
+#define FUNC_H_INCLUDED
+
+void change2LowerCase(char s);
+void change2UpperCase(char s);
+void switchCase(char s);
+void makeNames(char s);
+
+#endif // FUNC_H_INCLUDED
