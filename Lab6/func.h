5f49d87080b3941b0a464ccb3c55cce7b4162a2d
Patryk Szwed <patryk.szwed@outlook.com>
2017-11-23 02:49:27 +0100

    deleted:    Lab6/Lab6.cbp 	deleted:    Lab6/Lab6.depend 	deleted:    Lab6/Lab6.layout 	deleted:    Lab6/bin/Debug/InputLines.txt 	deleted:    Lab6/bin/Debug/Lab6.exe 	deleted:    Lab6/func.h 	deleted:    Lab6/funkcja.c 	deleted:    Lab6/main.c 	deleted:    Lab6/obj/Debug/func.o 	deleted:    Lab6/obj/Debug/main.o

--- Lab6/func.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNC_H_INCLUDED
-#define FUNC_H_INCLUDED
-
-void change2LowerCase(char s);
-void change2UpperCase(char s);
-void switchCase(char s);
-void makeNames(char s);
-
-#endif // FUNC_H_INCLUDED
