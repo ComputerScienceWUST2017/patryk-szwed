ad39d6b98eec515d173990193dac6689dc5fda29
Patryk Szwed <patryk.szwed@outlook.com>
2017-11-27 01:01:03 +0100

    Fixed incorrect folders and files

---
+++ IntroductionToProgramming/5/5/bin/Debug/lab5.bat
@@ -0,0 +1,9 @@
+1 mir < InputLines.txt
+1 "is w" < InputLines.txt
+2 "is wh" minut < InputLines.txt
+2 abc xyz < InputLines.txt
+3 - "is wha" < InputLines.txt
+3 - abc < InputLines.txt
+4 minut - < InputLines.txt
+4 kl - < InputLines.txt
+PAUSE
--- IntroductionToProgramming/5/5/main.c
+++ IntroductionToProgramming/5/5/main.c
@@ -4,6 +4,10 @@
 
 int main(int argc, char * argv[])
 {
+    const int N = 256;  //256 = line length limit
+
+    char line[N];
+
     while(RangeSelection2(argv[1], argv[2]))
     {
         continue;
